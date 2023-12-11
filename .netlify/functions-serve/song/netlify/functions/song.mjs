4edf5630302f91759a58fab32d75713a81382057
itochs <halglobe0108@gmail.com>
2023-12-11 15:33:59 +0900

    init

---
+++ .netlify/functions-serve/song/netlify/functions/song.mjs
@@ -0,0 +1,23 @@
+
+import {createRequire as ___nfyCreateRequire} from "module";
+import {fileURLToPath as ___nfyFileURLToPath} from "url";
+import {dirname as ___nfyPathDirname} from "path";
+let __filename=___nfyFileURLToPath(import.meta.url);
+let __dirname=___nfyPathDirname(___nfyFileURLToPath(import.meta.url));
+let require=___nfyCreateRequire(import.meta.url);
+
+
+// netlify/functions/song.js
+async function song_default() {
+  const html = `
+    <div id='rg_embed_link_3039923' class='rg_embed_link' data-song-id='3039923'>Read <a href='https://genius.com/Kendrick-lamar-humble-lyrics'>\u201CHUMBLE.\u201D by Kendrick\xA0Lamar</a> on Genius</div> <script crossorigin src='//genius.com/songs/3039923/embed.js'></script>`;
+  return new Response(html, {
+    headers: {
+      "Content-type": "text/html"
+    }
+  });
+}
+export {
+  song_default as default
+};
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsibmV0bGlmeS9mdW5jdGlvbnMvc29uZy5qcyJdLAogICJzb3VyY2VzQ29udGVudCI6IFsiZXhwb3J0IGRlZmF1bHQgYXN5bmMgZnVuY3Rpb24gKCkge1xuICAgIGNvbnN0IGh0bWwgPSBgXG4gICAgPGRpdiBpZD0ncmdfZW1iZWRfbGlua18zMDM5OTIzJyBjbGFzcz0ncmdfZW1iZWRfbGluaycgZGF0YS1zb25nLWlkPSczMDM5OTIzJz5SZWFkIDxhIGhyZWY9J2h0dHBzOi8vZ2VuaXVzLmNvbS9LZW5kcmljay1sYW1hci1odW1ibGUtbHlyaWNzJz5cdTIwMUNIVU1CTEUuXHUyMDFEIGJ5IEtlbmRyaWNrXHUwMEEwTGFtYXI8L2E+IG9uIEdlbml1czwvZGl2PiA8c2NyaXB0IGNyb3Nzb3JpZ2luIHNyYz0nLy9nZW5pdXMuY29tL3NvbmdzLzMwMzk5MjMvZW1iZWQuanMnPjwvc2NyaXB0PmBcbiAgICByZXR1cm4gbmV3IFJlc3BvbnNlKGh0bWwsIHtcbiAgICAgICAgaGVhZGVyczoge1xuICAgICAgICAgICAgJ0NvbnRlbnQtdHlwZSc6ICd0ZXh0L2h0bWwnXG4gICAgICAgIH1cbiAgICB9KVxufSJdLAogICJtYXBwaW5ncyI6ICI7Ozs7Ozs7Ozs7QUFBQSxlQUFPLGVBQTBCO0FBQzdCLFFBQU0sT0FBTztBQUFBO0FBRWIsU0FBTyxJQUFJLFNBQVMsTUFBTTtBQUFBLElBQ3RCLFNBQVM7QUFBQSxNQUNMLGdCQUFnQjtBQUFBLElBQ3BCO0FBQUEsRUFDSixDQUFDO0FBQ0w7IiwKICAibmFtZXMiOiBbXQp9Cg==
---
+++ netlify/functions/song.js
@@ -0,0 +1,9 @@
+export default async function () {
+    const html = `
+    <div id='rg_embed_link_3039923' class='rg_embed_link' data-song-id='3039923'>Read <a href='https://genius.com/Kendrick-lamar-humble-lyrics'>“HUMBLE.” by Kendrick Lamar</a> on Genius</div> <script crossorigin src='//genius.com/songs/3039923/embed.js'></script>`
+    return new Response(html, {
+        headers: {
+            'Content-type': 'text/html'
+        }
+    })
+}
