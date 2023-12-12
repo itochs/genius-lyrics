8b867a1f69bcc151edae140981e39b3e33f0c4ca
itochs <halglobe0108@gmail.com>
2023-12-12 09:45:31 +0900

    not use netlify functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
-function App() {
-  const [url, setUrl] = useState();
+const TOKEN =
+  "8cnhRTs-VGVNYkGIm82bVO_X-X78HGaOydsI2AFMci8r8TeH448Ph0NhjtNqn0fn";
 
+async function fetchSong(songId) {
+  const response = await fetch(`/api/songs/${songId}`, {
+    headers: { Authorization: `Bearer ${TOKEN}` },
+  });
+  return response.json();
+}
+
+async function fetchPage(url) {
+  const originalUrl = new URL(url);
+  const response = await fetch(`/html/${originalUrl.pathname}`);
+  return response.text();
+}
+
+function getLines(node, result) {
+  if (node.nodeType === node.TEXT_NODE) {
+    result.push(node.textContent);
+  }
+  if (node.nodeType === node.ELEMENT_NODE && node.tagName === "BR") {
+    result.push("\n");
+  }
+  for (const child of node.childNodes) {
+    getLines(child, result);
+  }
+}
+
+async function fetchLyrics(songId) {
+  const data = await fetchSong(songId);
+  const html = await fetchPage(data.response.song.url);
+  const doc = new DOMParser().parseFromString(html, "text/html");
+  const lines = [];
+  for (const container of doc.querySelectorAll(
+    'div[data-lyrics-container="true"]'
+  )) {
+    getLines(container, lines);
+    lines.push("\n");
+  }
+  return lines.join("");
+}
+
+export default function App() {
+  const [lyrics, setLyrics] = useState();
   useEffect(() => {
     (async () => {
-      setTimeout(() => {
-        setUrl("/.netlify/functions/song");
-      }, 2000);
+      const lyrics = await fetchLyrics(378195);
+      setLyrics(lyrics);
     })();
   }, []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <iframe src={url}></iframe>
-    </>
-  );
+  return <pre>{lyrics}</pre>;
 }
-
-export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://api.genius.com",
-      "/.netlify/functions": "http://localhost:9999/"
-    }
+      "/api": {
+        target: "https://api.genius.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+      "/html": {
+        target: "https://genius.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/html/, ""),
+      },
+    },
   },
   plugins: [react()],
-})
+});
